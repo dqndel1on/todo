3f4d2999308c8b77ce526e4e24371ad121656726
sanjipun <iamsanjipun@gmail.com>
2021-12-22 21:23:11 +0545

    add contract address in store

--- store/ethers.ts
+++ store/ethers.ts
@@ -3,11 +3,13 @@ import create from 'zustand'
 type EthersType = {
     provider: boolean | any,
     account: [],
+    contractAddress: string,
     setProvider: (data) => void,
     setAccount: (data) => void
 }
 
 const useEthers = create<EthersType>(set => ({
+    contractAddress: '0xfd1DDAaC4A9b5E2A05BBe685A7e2aA446eDeFa83',
     provider: false,
     account: [],
     setProvider: (data) => set({ provider: data }),
