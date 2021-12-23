ddff64b7d2919a8055f7c398c78c553f3c08f4cb
sanjipun <iamsanjipun@gmail.com>
2021-12-23 23:18:45 +0545

    add ethers.store.ts

---
+++ store/ethers.store.ts
@@ -0,0 +1,36 @@
+import { ethers } from 'ethers';
+import create from 'zustand'
+
+declare let window: any;
+
+type EthersType = {
+    accounts: string[],
+    contractAddress: string,
+    setAccounts: (data) => void,
+    requestAccounts: () => void,
+    checkMMConnection: () => void
+}
+
+const useEthers = create<EthersType>((set, get) => ({
+    contractAddress: '0xfd1DDAaC4A9b5E2A05BBe685A7e2aA446eDeFa83',
+    accounts: [''],
+    setAccounts: (data) => set({ accounts: data }),
+    requestAccounts: async () => {
+        const Accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        set({ accounts: Accounts })
+    },
+    checkMMConnection: async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        if (provider) {
+            const accounts = await provider.listAccounts();
+            if (accounts.length !== 0) {
+                void get().requestAccounts();
+            } else {
+                set({ accounts })
+            }
+
+        }
+    }
+}))
+
+export default useEthers;
